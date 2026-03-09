ed125150d9844382a6c01b42465486f7854fab2e
th <th@thnoMac-mini.local>
2026-03-09 16:53:13 +0900

    update test case

---
+++ com.jhappy.jhappyloveany.test/src/com/jhappy/jhappyloveany/test/UIXmlTest.java
@@ -0,0 +1,28 @@
+package com.jhappy.jhappyloveany.test;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.jhappy.jhappyloveany.test.util.TestUtil;
+
+@DisplayName("UI XML Test: UIperformTest")
+
+class UIXmlTest extends SimpleJDTBuildTest {
+
+	@Test
+	public void execTest() throws Exception {
+		
+		//テスト用のサンプルファイルが保存してあるフォルダ
+		final String TEST_RESOURCES_FOLDER = "test-resources/samplefiles2";
+
+		TestUtil.doTest(BUNDLE_NAME,project, TEST_RESOURCES_FOLDER, "src/com/test/dir0/dir1/config_1.xml",
+				List.of("xml_f10", "xml_f101_e0", "xml_f200_e0"));
+
+		// ※ syncExec の外にあった Thread.sleep は削除しました
+	}
+
+	
+
+}
